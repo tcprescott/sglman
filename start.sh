4bf4f557f93479f6cd3b12f0818cbaf6b7e8b43a
Thomas Prescott <tcprescott@gmail.com>
2025-09-25 16:56:36 -0500

    fix start.sh

--- start.sh
+++ start.sh
@@ -8,14 +8,13 @@ if [ -f .env ]; then
     export $(grep -v '^#' .env | xargs)
 fi
 
+
 if [ "$1" = "prod" ]; then
     echo "Starting Uvicorn server in production mode..."
-    # we also use a single worker in production mode so socket.io connections are always handled by the same worker
-    uvicorn main:app --workers 1 --log-level info --port 8000 --host 0.0.0.0
+    poetry run uvicorn main:app --workers 1 --log-level info --port 8000 --host 0.0.0.0
 elif [ "$1" = "dev" ]; then
     echo "Starting Uvicorn server in development mode..."
-    # reload implies workers = 1
-    uvicorn main:app --reload --log-level debug --port 8000
+    poetry run uvicorn main:app --reload --log-level debug --port 8000
 else
     echo "Invalid parameter. Use 'prod' or 'dev'."
     exit 1
